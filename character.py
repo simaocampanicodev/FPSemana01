42cde8e64d0aa7f3efad23cf01ee56a0ce0d4b6e
Simão Campaniço <simaocampanico.dev@gmail.com>
2024-11-08 11:18:11 +0000

    Adding character.py exercise

---
+++ character.py
@@ -0,0 +1,26 @@
+characters = []
+count = 3
+for i in range (count):
+    name = str(input(""))
+    attack = int(input(""))
+    defense = int(input(""))
+    character = [name, attack, defense]
+    characters.append(character)
+    
+print (characters)
+
+max_attack = -1
+max_defense = -1
+max_attack_character = None
+max_defense_character = None
+
+for character in characters:
+    if character[1] > max_attack:
+        max_attack = character[1]
+        max_attack_character = character
+    if character[2] > max_defense:
+        max_defense = character[2]
+        max_defense_character = character
+
+print(("Ataque ") + str(max_attack_character[0]) + " "  + str(max_attack_character[1]))
+print(("Defesa ") + str(max_defense_character[0]) + " " + str(max_defense_character[2]))
